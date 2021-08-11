20ed0ccb8e81026f0817892007fe8d4d8db901a8
exether <yocrudo@gmail.com>
2021-08-11 17:30:48 -0500

    Segundo commit, no hay muchos cambios, apenas se está creando la página, migraciones de bdd, tailwind css framework

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/posts', function () {
+    //Si ponemos algo después de la diagonal, es como poner una dirección web, sólo si escribes localchost/posts aparecerá lo que pusiste en el view
+    //return view('welcome');
+    return view('posts.index');
+    //posts es la carpeta contenedora, usamos un punto en lugar de una diagonal para acceder a otra carpeta o a un archivo, es una dirección
 });
